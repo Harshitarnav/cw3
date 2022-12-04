5f6bda9c4457080103b2ea18108fe16dc887955f
s1951693 <s1951693@ed.ac.uk>
2022-12-04 21:02:40 +0000

    Part 1 done

--- l4state.py
+++ l4state.py
@@ -44,10 +44,7 @@ class L4State14(app_manager.RyuApp):
 
         tcph = pkt.get_protocols(tcp.tcp)
         iph = pkt.get_protocols(ipv4.ipv4)
-        dst, src = (eth.dst, eth.src)
-
-        # print(in_port, tcph)
-        # print(in_port, iph)
+        # dst, src = (eth.dst, eth.src)
 
         if (len(tcph)==0 or len(iph)==0):
             if in_port == 1:
@@ -55,7 +52,7 @@ class L4State14(app_manager.RyuApp):
             elif in_port==2:
                 acts = [psr.OFPActionOutput(1)]
         
-        elif eth.ethertype == ETH_TYPE_IP:
+        if eth.ethertype == ETH_TYPE_IP and len(tcph)>0:
             srcip = iph[0].src
             dstip = iph[0].dst
             srcport = tcph[0].src_port
@@ -65,7 +62,7 @@ class L4State14(app_manager.RyuApp):
                 if tcph[0].has_flags(tcp.TCP_FIN, tcp.TCP_SYN) or tcph[0].has_flags(tcp.TCP_RST, tcp.TCP_SYN) or tcph[0].bits==0:
                     acts = [psr.OFPActionOutput(ofp.OFPPC_NO_FWD)]
 
-                elif ((srcip, dstip, srcport, dstport)) not in self.ht:
+                else:# if ((srcip, dstip, srcport, dstport)) not in self.ht:
                     self.ht.add((srcip, dstip, srcport, dstport))
                     acts = [psr.OFPActionOutput(2)]
                     mtc = psr.OFPMatch(in_port=in_port, ipv4_src = srcip, ipv4_dst = dstip, tcp_src = srcport, tcp_dst = dstport)
@@ -83,11 +80,7 @@ class L4State14(app_manager.RyuApp):
                 
                 else:
                     acts = [psr.OFPActionOutput(ofp.OFPPC_NO_FWD)]
-        else:
-            if in_port == 1:
-                acts = [psr.OFPActionOutput(2)]
-            elif in_port==2:
-                acts = [psr.OFPActionOutput(1)]
+                    
         #
         data = msg.data if msg.buffer_id == ofp.OFP_NO_BUFFER else None
         out = psr.OFPPacketOut(datapath=dp, buffer_id=msg.buffer_id,
